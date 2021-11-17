3388a02a15e8ec73790584ee5f299a6ef4ebac35
JimBeakIt <87563663+JimBeakIt@users.noreply.github.com>
2021-11-17 09:18:21 +0000

    update

--- Program.cs
+++ Program.cs
@@ -8,8 +8,32 @@ namespace ClassesAndMethodsAssignment
 {
     class Program
     {
+        public static int Data { get; set; }
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter a number:");
+            string line = Console.ReadLine();
+            Data = Int32.Parse(line);
+            var operatorObject = new mathInt();
+            var result = operatorObject.GetAdd(Data);
+            Console.WriteLine(result);
+            Console.ReadLine();
+
+            Console.WriteLine("Enter a number:");
+            string line2 = Console.ReadLine();
+            Data = Convert.ToInt32(line2);
+            var operatorObject2 = new mathInt();
+            var result2 = operatorObject2.GetSubtract(Data);
+            Console.WriteLine(result2);
+            Console.ReadLine();
+
+            Console.WriteLine("Enter a number:");
+            string line3 = Console.ReadLine();
+            Data = Convert.ToInt32(line2);
+            var operatorObject3 = new mathInt();
+            var result3 = operatorObject2.GetMultiply(Data);
+            Console.WriteLine(result3);
+            Console.ReadLine();
         }
     }
 }
---
+++ mathInt.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndMethodsAssignment
+{
+    public class mathInt
+    {
+        public int GetAdd(int data)
+        {
+            data = data + 4;
+            return data;
+        }
+
+        public decimal GetSubtract(decimal data)
+        {
+            data = data - 3;
+            decimal Subt = Convert.ToInt32(data);           
+            return Subt;
+        }
+
+        public int GetMultiply(int data)
+        {
+            data = data * 4;
+            int Mult = Convert.ToInt32(data);
+            return Mult;
+        }
+    }
+}
